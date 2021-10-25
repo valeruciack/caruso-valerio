9058e876c0c48b1d5ec4e9ab1da31ac06cdfd5f2
valeruciack <92056476+valeruciack@users.noreply.github.com>
2021-10-25 20:06:38 +0200

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,20 @@
+Python 3.10.0 (v3.10.0:b494f5935c, Oct  4 2021, 14:59:20) [Clang 12.0.5 (clang-1205.0.22.11)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+x= 18
+y_ 3
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+y= 3
+x+y
+21
+x-y
+15
+x*y
+54
+x/y
+6.0
+x%y
+0
+x**y
+5832
+x//y
+6
